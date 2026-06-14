119242fef024c5706c9f801fc6691d487aef899c
Anthony Pin <anthonypin0169@gmail.com>
2026-06-14 12:04:36 +0200

    résolutions de bugs dans le footer

--- front/src/component/footer.jsx
+++ front/src/component/footer.jsx
@@ -18,16 +18,23 @@ export default function Footer() {
                     <li className="footer-container__planning--li"><span>Samedi</span><span>09:00-19:00</span></li>
                     <li className="footer-container__planning--li"><span>Dimanche</span><span>Fermé</span></li>
                 </div>
-                <div className="footer-container__links">
-                    <NavLink text="Contactez nous" to="/contact" className="footer-container__links--contact"/>
-                    <NavLink text="Mentions légales" to="/mentions" className="footer-container__links--mentions"/>
-                    <NavLink text="Conditions générales de ventes" to="/conditions"     className="footer-container__links--conditions"/>
-                </div>
-                <div className="footer-container__follows">
-                    <Logo src={instagramLogo} alt={"Logo Instagram"} className="footer-container__follows--instagram"/>
-                    <Logo src={facebookLogo} alt={"Logo Facebook"} className="footer-container__follows--facebook"/>
-                </div>
             </div>
+                <div className="footer__links">
+                    <NavLink text="Contactez nous" to="/contact" className="footer__links--contact"/>
+                    <NavLink text="Mentions légales" to="/mentions" className="footer__links--mentions"/>
+                    <NavLink text="Conditions générales de ventes" to="/conditions"     className="footer__links--conditions"/>
+                </div>
+                <div className="footer__follows">
+                    <div className="footer__follows--instagram">
+                        <Logo src={instagramLogo} alt={"Logo Instagram"} className="instagram-icon"/>
+                        <p>Rejoignez nous sur Instagram</p>
+                    </div>                        
+                    <div className="footer__follows--facebook">
+                        <Logo src={facebookLogo} alt={"Logo Facebook"} className="facebook-icon"/>
+                        <p>Rejoignez nous sur Facebook</p>
+                    </div>        
+                </div>
+            
         </footer>
     )
 }
