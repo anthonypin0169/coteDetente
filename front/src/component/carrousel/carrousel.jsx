636957da8b3bdedda03eb09529c14da41c2ec4b5
Anthony Pin <anthonypin0169@gmail.com>
2026-07-31 19:04:27 +0200

    Réécriture de la logique du carrousel pour ajouter une animation de slide au dessus de 1024px.

--- front/src/component/carrousel/carrousel.jsx
+++ front/src/component/carrousel/carrousel.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useCallback } from "react"
+import googleLogo from "../../assets/images/google-logo.png"
 import "./carrousel.scss"
 
 export default function Carrousel({images = [], reviews = [], slides = [], mode, className}) {
@@ -30,7 +31,7 @@ export default function Carrousel({images = [], reviews = [], slides = [], mode,
     }
     ,[currentIndex, mode, next]
     )
-    
+
         return (
             <div className={`carrousel ${className ?? ""}`}>
                 {slides.length > 0 ? (
@@ -38,15 +39,36 @@ export default function Carrousel({images = [], reviews = [], slides = [], mode,
                 ) : images.length > 0 ? (
                     <img src={images[currentIndex]} alt={`slide ${currentIndex}`} className="carrousel__image" />
                 ) : (
-                    <div className="carrousel__review">
-                        <h3 className="carrousel__review--title">{reviews[currentIndex]?.title}</h3>
-                        <p className="carrousel__review--text">{reviews[currentIndex]?.text}</p>
-                    </div>
-                )}
+                    reviews.map((review, i) => { 
+                        
+                        const position = 
+                        i === currentIndex ? "center" :   
+                        i === (currentIndex - 1 + items.length) % items.length ? "left" :
+                        i === (currentIndex + 1) % items.length ? "right" : 
+                        "hide"
+                            
+                        return ( 
+                            <div className={`carrousel__review ${position}`} key={i}>
+                                <div className="carrousel__review--name-group">
+                                    <div className="carrousel-review-avatar">{review.name?.charAt(0)}</div>
+                                    <h3 className="carrousel-review-name">{review.name}</h3>
+                                    <img src={googleLogo} alt="Google" className="carrousel__review--google"/>
+                                </div>
+
+                                <div className="carrousel__review--text-group">
+                                    <span className="carrousel-review-rating">
+                                        {Array.from({length: review.rating}, (_, i) => (
+                                        <i key={i} className="fa-solid fa-star"></i>))}
+                                    </span>
+                                    <p className="carrousel-review-text">{review.text}</p>
+                                </div>
+                            </div>
+                        )}) 
+                    )}
                 {mode === "manual" && (
                     <>
-                        <button className="carrousel__btn--prev" onClick={prev}>&#8249;</button>
-                        <button className="carrousel__btn--next" onClick={next}>&#8250;</button>
+                        <button className="carrousel__btn--prev" onClick={prev}><i className="fa-solid fa-chevron-left"></i></button>
+                        <button className="carrousel__btn--next" onClick={next}><i className="fa-solid fa-chevron-right"></i></button>
                     </>
                 )}
             </div>
--- front/src/pages/home/components/CustomerReviews.jsx
+++ front/src/pages/home/components/CustomerReviews.jsx
@@ -5,24 +5,24 @@ import "./CustomerReviews.scss"
 export default function CustomerReviews() {
     return (
         <section className="home__customers-review">
+            <div className="home__customers-review--reviews">
+                <h2>Nos avis clients</h2>
+                <Carrousel mode={"manual"} reviews = {[
+                    {name: "Fany D.",rating: 5, text: "Pas de mot pour dire combien c'était parfait. L'équipe au petit soin, le massage exactement ce qu'il me fallait, les produits avec une odeur de dingue et de super qualité. Merci à mes collègues (en or), ça fait partie des cadeaux dont je me souviendrais longtemps, et merci à l'équipe de côté détente."},
+                    {name: "Aurélie B.",rating: 5, text: "Merci à sabine et à toute son équipe pour leurs professionnalismes et leurs suivis. Grâce à elles j’ai réussi à perdre une dizaine de kilos avant mon mariage avec un programme adapté, ps: merci aussi pour votre touche esthétique.. (beautés des mains avec gel, beauté des pieds avec semi permanent et soin du visage pour le jour J) Je recommande ++++++"},
+                    {name: "Julie R.",rating: 5, text: "Institut très agréable, accueil toujours chaleureux. Les personnes sont disponibles, professionnelles, à l'écoute. Je recommande vivement leurs prestations."},
+                    {name: "Michelle B.",rating: 5, text: "Je fréquente cet Institut depuis de nombreuses années pour différentes prestations : les filles sont sympas, sérieuses. Jamais déçue, je recommande cet établissement"},
+                    {name: "Julie S.",rating: 5, text: "Une equipe aux petits soins. Discrètes, professionnelles et de bons conseils. Je recommande cet institut que ce soit pour l épilation, les soins ou les ongles"},
+                    {name: "Isabelle M.",rating: 5, text: "Ça fait 12 ans que je viens chez Côté Détente. Service personnalisé, Toujours le sourire. C'est un plaisir de venir pour un résultat toujours impeccable"},
+                    {name: "Murielle C.",rating: 5, text: "Je me suis vu offrir un soin du visage Decleor pour mon anniversaire; un moment de pure détente, l'esthéticienne très professionnelle, douce, à l'écoute de mes souhaits, des messages de relaxation délivrés durant le soin aidant au lâcher prise....Très bien!. Les produits utilisés très onctueux, des senteurs agréables. Le cadre agréable lui aussi et reposant. Une belle découverte."},
+                    {name: "Charlène B.",rating: 5, text: "Encore un grand merci à l'institut. Une heure de massage, un pur moment de détente qui m'a fait énormément de bien. Merci à l'esthéticienne qui a été au petit soin et respectueuse des demandes. Je recommande !"},
+                    {name: "Sarah A.",rating: 5, text: "Équipe très professionnelle, un institut où l’on est très bien accueillie et conseillée! Les gammes de produits utilisées sont de grande qualité, un savoir faire unique. Merci à Sabine et à ses collaboratrices !"}
+                ]}  className="home__customers-review--carrousel" />
+            </div>
             <div className="home__customers-review--info">
                 <h2>Pour nous retrouver</h2>
                 <img src={map} alt="carte" className="home__customers-review--map" />
                 <h3 className="home__customers-review--text">261 Rue de Schutterwald, 01000 Saint-Denis-lès-Bourg</h3>
-            </div>
-            <div className="home__customers-review--reviews">
-                <h2>Nos avis clients</h2>
-                <Carrousel mode={"manual"} reviews = {[
-                    {title: "Fany D.", text: "Pas de mot pour dire combien c'était parfait. L'équipe au petit soin, le massage exactement ce qu'il me fallait, les produits avec une odeur de dingue et de super qualité. Merci à mes collègues (en or), ça fait partie des cadeaux dont je me souviendrais longtemps, et merci à l'équipe de côté détente."},
-                    {title: "Aurélie B.", text: "Merci à sabine et à toute son équipe pour leurs professionnalismes et leurs suivis. Grâce à elles j’ai réussi à perdre une dizaine de kilos avant mon mariage avec un programme adapté, ps: merci aussi pour votre touche esthétique.. (beautés des mains avec gel, beauté des pieds avec semi permanent et soin du visage pour le jour J) Je recommande ++++++"},
-                    {title: "Julie R.", text: "Institut très agréable, accueil toujours chaleureux. Les personnes sont disponibles, professionnelles, à l'écoute. Je recommande vivement leurs prestations."},
-                    {title: "Michelle B.", text: "Je fréquente cet Institut depuis de nombreuses années pour différentes prestations : les filles sont sympas, sérieuses. Jamais déçue, je recommande cet établissement"},
-                    {title: "Julie S.", text: "Une equipe aux petits soins. Discrètes, professionnelles et de bons conseils. Je recommande cet institut que ce soit pour l épilation, les soins ou les ongles"},
-                    {title: "Isabelle M.", text: "Ça fait 12 ans que je viens chez Côté Détente. Service personnalisé, Toujours le sourire. C'est un plaisir de venir pour un résultat toujours impeccable"},
-                    {title: "Murielle C.", text: "Je me suis vu offrir un soin du visage Decleor pour mon anniversaire; un moment de pure détente, l'esthéticienne très professionnelle, douce, à l'écoute de mes souhaits, des messages de relaxation délivrés durant le soin aidant au lâcher prise....Très bien!. Les produits utilisés très onctueux, des senteurs agréables. Le cadre agréable lui aussi et reposant. Une belle découverte."},
-                    {title: "Charlène B.", text: "Encore un grand merci à l'institut. Une heure de massage, un pur moment de détente qui m'a fait énormément de bien. Merci à l'esthéticienne qui a été au petit soin et respectueuse des demandes. Je recommande !"},
-                    {title: "Sarah A.", text: "Équipe très professionnelle, un institut où l’on est très bien accueillie et conseillée! Les gammes de produits utilisées sont de grande qualité, un savoir faire unique. Merci à Sabine et à ses collaboratrices !"}
-                ]}  className="home__customers-review--carrousel" />
             </div>
         </section>
     )
