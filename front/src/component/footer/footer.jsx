318ead32a00226584ab19100fa2b10f140f9c7ae
Anthony Pin <anthonypin0169@gmail.com>
2026-08-02 18:44:28 +0200

    Ajout de la map dans le footer, css footer fini sauf détails

--- front/src/component/footer/footer.jsx
+++ front/src/component/footer/footer.jsx
@@ -1,5 +1,7 @@
+import { useRef, useEffect } from "react"
 import NavLink from "../nav/nav"
-import map from "@/assets/images/map.png"
+import L from "leaflet"
+import "leaflet/dist/leaflet.css"
 import Logo from "../logo/logo"
 import institutLogo from "../../assets/images/testLogo2.png"
 import instagramLogo from "../../assets/images/instagram-logo.png"
@@ -7,29 +9,40 @@ import facebookLogo from "../../assets/images/facebook-logo.png"
 import "./footer.scss"
 
 export default function Footer() {
+
+    const mapRef = useRef(null)
+    useEffect(() => {
+        const initialMap = L.map(mapRef.current).setView([46.200537, 5.192188], 16)
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(initialMap)
+        L.marker([46.200537, 5.192188]).addTo(initialMap)
+        return () => initialMap.remove()
+    },[])
+
     return (
         <footer className="footer">
             <Logo src={institutLogo} alt={"Logo Coté détente"} className="footer__institut-icon"/>
 
             <div className="footer__map-container">
                 <h2>Pour nous retrouver</h2>
-                <img src={map} alt="carte" className="footer__map-container--img" />
+                <div ref={mapRef} className="footer__map-container--img"></div>
                 <h3 className="footer__map-container--text">261 Rue de Schutterwald, 01000 Saint-Denis-lès-Bourg</h3>
             </div>
 
             <div className="footer__content-container">
                 <div className="footer__content-container--planning">
-                    <h4 className="footer__content-container">Nos horaires :</h4>
-                    <li className="footer__content-container"><span>Du Mardi au Samedi</span><span>09:00-19:00</span></li>
-                    <li className="footer__content-container"><span>Lundi et Dimanche</span><span>Fermé</span></li>
+                    <h4 className="footer-content-container-title">Nos horaires :</h4>
+                    <li className="content-container-li"><span>Du Mardi au Samedi</span><span>09:00-19:00</span></li>
+                    <li className="content-container-li"><span>Lundi et Dimanche</span><span>Fermé</span></li>
                 </div>
 
                 <div className="footer__content-container--links">
                     <h4 className="footer__links--h4">Liens utiles :</h4>
-                    <NavLink text="Accueil" to="#home-top-carrousel"/>
-                    <NavLink text="Contactez nous" to="/contact"/>
-                    <NavLink text="Mentions légales" to="/mentions"/>
-                    <NavLink text="Conditions générales de ventes" to="/conditions"     className="footer__links--conditions"/>
+                    <NavLink text="Accueil" to="#home-top-carrousel" className="footer-navlink"/>
+                    <NavLink text="Contactez nous" to="/contact" className="footer-navlink"/>
+                    <NavLink text="Mentions légales" to="/mentions" className="footer-navlink"/>
+                    <NavLink text="Conditions générales de ventes" to="/conditions"      className="footer-navlink"/>
                     <a 
                     href="https://www.instagram.com/institutcotedetente_/" 
                     target="blank"
