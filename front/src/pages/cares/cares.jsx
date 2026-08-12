e85afccebec6a916c6cad7b5461048ab9056d3ed
Anthony Pin <anthonypin0169@gmail.com>
2026-08-12 22:08:35 +0200

    Fonctionnement de la modale de modification des pages de prestation partiellement ajoutée, reste vue 3 à finaliser et css.

--- front/src/pages/cares/cares.jsx
+++ front/src/pages/cares/cares.jsx
@@ -1,25 +1,32 @@
 import "./cares.scss"
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
+import { useSelector } from "react-redux"
+import Modal from "@/component/modal/modal"
 
 export default function Cares() {
 
     const { sousTypeSlug } = useParams()
+    const token = useSelector((state) => state.auth.token)
 
 
-    /* Récuperer les sous types et mettre à jour sur le sous type actuel */
-    const [currentSousType, setCurrentSousType] = useState(null)
+    /* Récuperer les sous types, mettre à jour sur le sous type actuel et modifier son titre */
+    const [allSousTypes, setAllSousTypes] = useState([])
+    const [currentSousType, setCurrentSousType] = useState("")
+    const [actualSousTypeName,setActualSousTypeName] = useState("")
+
     useEffect(()=>{
         const loadSousTypes = async () => {
             try {
-                const typesResponse = await fetch ("/api/sous-types")
-                const typesResponseJson = await typesResponse.json()
+                const sousTypesResponse = await fetch ("/api/sous-types")
+                const sousTypesResponseJson = await sousTypesResponse.json()
 
-                if(!typesResponseJson){
+                if(!sousTypesResponseJson){
                     throw new Error ("erreur dans la récuperation des sous-types")
                 }
-                const found = typesResponseJson.find(sousType => sousType.route === `/soins/${sousTypeSlug}`)
+                const found = sousTypesResponseJson.find(sousType => sousType.route === `/soins/${sousTypeSlug}`)
                 setCurrentSousType(found)
+                setAllSousTypes(sousTypesResponseJson)
 
             }catch(error){
                 (error.message)
@@ -28,6 +35,34 @@ export default function Cares() {
         loadSousTypes()
     },[sousTypeSlug])
 
+
+    /* Modifier le nom d'un sous-type et n'en cibler qu'un seul */
+    const handleUpdateSousType = async (id) => {
+        
+        const formData = new FormData()
+        formData.append("name", actualSousTypeName)
+
+        try{
+            const uploadTitle = await fetch (`/api/sous-types/${id}`,{
+                method : "PUT",
+                body : formData,
+                headers : { Authorization : `Bearer ${token}`}
+            })
+            const updatedTitle = await uploadTitle.json()
+
+            if (uploadTitle.ok){
+                setActualSousTypeName(updatedTitle.name)
+                setAllSousTypes(prev => prev.map(t => t._id === updatedTitle._id ? updatedTitle : t))
+
+            }
+
+        }catch(error){
+                (error.message)
+            }
+    }
+
+    const [editingSousTypeId, setEditingSousTypeId] = useState(null)
+    
 
     /* Récuperer les groupes d'un sous-type */
     const [groups, setGroups] = useState([])
@@ -74,11 +109,46 @@ export default function Cares() {
     },[groups])
 
 
+    /* Boite modale */
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+    const [modalVue, setModalVue] = useState("sousTypes")
+    const [selectedSousTypeId, setSelectedSousTypeId] = useState("")
+    const [modalGroups, setModalGroups] = useState([])
+
+    useEffect(() => {
+        if (!selectedSousTypeId) return
+
+        const loadSelectedSousType = async () => {
+            try{
+                const response = await fetch (`/api/groups/sous-type/${selectedSousTypeId}`)
+                const responseJson = await response.json()
+
+                if(!responseJson){
+                    throw new Error ("erreur dans la récuperation du sous-type")
+                }
+                setModalGroups(responseJson)
+
+            }catch(error){
+                (error.message)
+            }
+        }
+        loadSelectedSousType()
+    },[selectedSousTypeId])
+
+
+    /* Vue 3 */
+    const [selectedGroupId, setSelectedGroupId] = useState("")
+
+
     return (
         <main className="cares">
          <h1 className="cares__title">Découvrez nos soins</h1>
 
             <section className="cares__section">
+                {isAuthenticated &&
+                    <button onClick={() => setModalIsOpen(true)} className="btn">Modifier</button>
+                }
                 {groups.map((bloc, i) => (
                     <div className="cares__section--type" key={bloc._id}>
                         <div className="presenting">
@@ -104,6 +174,70 @@ export default function Cares() {
                     </div>
                 ))}
             </section>
+
+            <Modal isOpen={modalIsOpen} onClose={() => {setModalIsOpen(false) ; setModalVue("sousTypes")}} variant="modify" >
+                {modalVue === "sousTypes" ?
+                    <div>
+                        {allSousTypes.map((type)=>(
+                            <div key={type._id}>
+                                {editingSousTypeId === type._id ?
+                                    <div>
+                                        <label htmlFor="sous-type-title">Choisir un titre :</label>
+                                        <input id="sous-type-title" type="text" value={actualSousTypeName} onChange={(e)=> setActualSousTypeName(e.target.value)}/>
+                                        <div>
+                                            <button onClick={() => setEditingSousTypeId(null)}>Retour</button>
+                                            <button onClick={() => {handleUpdateSousType(type._id) ; setEditingSousTypeId(false)}}>Valider le titre</button>
+                                            <button onClick={() => {setModalVue("groups"); setSelectedSousTypeId(type._id)}}>Modifier le contenu</button>
+                                        </div>
+                                    </div>
+                                :
+                                    <div>
+                                        <p>{type.name}</p>
+                                        <div>
+                                            <button onClick={() => {setEditingSousTypeId(type._id) ; setActualSousTypeName(type.name)}}>Modifier le titre</button>
+                                            <button onClick={() => {setModalVue("groups"); setSelectedSousTypeId(type._id)}}>Modifier le contenu</button>
+                                        </div>
+                                    </div>
+                                }
+                            </div>
+                        ))}
+                    </div>
+                : modalVue === "groups" ?
+                    <div>
+                        {modalGroups.map((group) => (
+                            <div key={group._id}>
+                                <label htmlFor="title-edit">Modifier le nom du groupe</label>
+                                <input type="text" id="title-edit"/>
+                                <label htmlFor="description-edit">Modifier la description</label>
+                                <input type="text" id="description-edit"/>
+                                <label htmlFor="photo-edit">Modifier la photo</label>
+                                <input type="file" id="photo-edit"/>
+                                <button onClick={() => {setModalVue("prestations") ; setSelectedGroupId(group._id)}} className="btn">Modifier les prestations</button>
+                            </div>
+                        ))}
+                        <div>
+                            <button onClick={() => setModalVue("sousTypes")} className="btn">Retour</button>
+                            <button className="btn">Ajouter un groupe</button>
+                        </div>
+                    </div>
+                : 
+                    <div>
+                        {prestations.filter(p => p.group === selectedGroupId).map((presta) => {
+                            <div key={presta._id}>
+                                <input id="presta-name" type="text" value={presta.name}/>
+                                <input id="presta-price" type="text" value={presta.price}/>
+                                <input id="presta-time" type="text" value={presta.duration}/>
+
+                                <button>Supprimer la prestation</button>
+                            </div>
+                        })}
+                        <div>
+                            <button>Retour</button>
+                            <button>Ajouter une prestation</button>
+                        </div>
+                    </div>
+                }
+            </Modal>
         </main>
     )
 }
