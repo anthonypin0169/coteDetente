4532809436c02719a88571f00463e58702f0408f
Anthony Pin <anthonypin0169@gmail.com>
2026-09-22 20:51:22 +0200

    Ajustement en 16:9 du carrousel

--- front/src/pages/contact/contact.jsx
+++ front/src/pages/contact/contact.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react"
+import { useState, useEffect } from "react"
 import { useSelector } from "react-redux"
 import Modal from "@/component/modal/modal"
 import SeoHead from "@/component/seoHead/seoHead"
